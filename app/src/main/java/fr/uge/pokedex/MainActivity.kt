f8c12c4b466cc6537a9572a5f7934b97ee8aa73a
Mateusz Szczech <szczenalol@gmail.com>
2023-02-11 16:48:35 +0100

    BottomNavigationMenu working with current screen highlight

--- app/src/main/java/fr/uge/pokedex/MainActivity.kt
+++ app/src/main/java/fr/uge/pokedex/MainActivity.kt
@@ -1,15 +1,17 @@
 package fr.uge.pokedex
 
+
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import fr.uge.pokedex.components.BottomNavigationMenu
+import fr.uge.pokedex.components.NavigationGraph
 import fr.uge.pokedex.ui.theme.PokedexTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +19,21 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PokedexTheme {
-                // A surface container using the 'background' color from the theme
+
+                val navController: NavHostController = rememberNavController()
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+
+                    Scaffold(bottomBar = { BottomNavigationMenu(navController)}) {
+                        Log.d("Padding",it.toString())
+                        NavigationGraph(navController = navController)
+                    }
                 }
+                
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PokedexTheme {
-        Greeting("Android")
     }
 }
