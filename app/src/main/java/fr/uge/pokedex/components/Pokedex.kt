16af673c63928ce385f7d02c00f905e7c33fa24e
Zineb SDAF <sdafzineb1s2@gmail.com>
2023-02-14 20:23:00 +0100

    Types avec des images et adaptation pour avoir toujours 2 cartes à l'horizontale

--- app/src/main/java/fr/uge/pokedex/components/Pokedex.kt
+++ app/src/main/java/fr/uge/pokedex/components/Pokedex.kt
@@ -2,7 +2,10 @@ package fr.uge.pokedex.components
 
 
 
+import android.content.ContentValues.TAG
 import android.content.Context
+import android.content.res.Resources
+import android.util.DisplayMetrics
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -23,42 +26,65 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-
 import fr.uge.pokedex.data.Pokemon
 import fr.uge.pokedex.data.PokemonRepository
 import fr.uge.pokedex.ui.theme.PokedexTheme
+
 
 @Composable
 fun DisplayPokedex(context: Context, pokemons: List<Pokemon>) {
     var clicked by rememberSaveable { mutableStateOf(false) }
 
-    LazyVerticalGrid(columns = GridCells.Adaptive(100.dp), horizontalArrangement = Arrangement.spacedBy(10.dp), verticalArrangement = Arrangement.spacedBy(10.dp), contentPadding = PaddingValues(50.dp, 50.dp)) {
+
+//(Resources.getSystem().displayMetrics.widthPixels/8).dp
+    LazyVerticalGrid(columns = GridCells.Fixed(2), horizontalArrangement = Arrangement.spacedBy(40.dp), verticalArrangement = Arrangement.spacedBy(50.dp), contentPadding = PaddingValues(30.dp, 30.dp)) {
         items(pokemons) {
             Column(
                 modifier = Modifier
-                    .fillMaxSize()
+
                     .border(2.dp, Color.Black)
                     .clickable {
                         if (!clicked) {
                             clicked = true
                         }
-                    }, horizontalAlignment = Alignment.CenterHorizontally,
+                    }, horizontalAlignment = Alignment.CenterHorizontally
             ) {
 
                 Image(
                     painter = painterResource(it.getIcon(context)),
+                    alignment = Alignment.Center,
                     contentDescription = it.name,
                     modifier = Modifier
                         .width(100.dp)
                         .height(100.dp)
+                        .border(2.dp, Color.Black)
+                        .padding(0.dp, 20.dp)
                 )
 
-                Text(text = "N°" + String.format("%03d", it.id) + " " + it.name)
-                Text(text = it.type.first.toString() + if (it.type.second.toString() == "NONE") "" else " - " + it.type.second.toString())
+                Text(text = "N°" + String.format("%03d", it.id) + " " + it.name, modifier = Modifier.padding(0.dp,20.dp))
+                Row() {
+                    Image(
+                        painter = painterResource(context.resources.getIdentifier("type_${it.type.first.toString().lowercase()}", "drawable", context.packageName)),
+                        contentDescription = it.type.first.toString(),
+                        modifier = Modifier
+                            .width(60.dp)
+                            .height(60.dp)
+
+                    )
+
+                    if(it.type.second.toString() != "NONE"){
+                        Image(
+                            painter = painterResource(context.resources.getIdentifier("type_${it.type.second.toString().lowercase()}", "drawable", context.packageName)),
+                            contentDescription = it.type.second.toString(),
+                            modifier = Modifier
+                                .width(60.dp)
+                                .height(60.dp)
+
+                        )
+                    }
+                }
 
             }
-
-
         }
     }
 }
