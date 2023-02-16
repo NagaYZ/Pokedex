e23f34e5a5a54cc7437d32f5843d3753efac4045
Zineb SDAF <sdafzineb1s2@gmail.com>
2023-02-16 16:34:31 +0100

    Grille Pokedex affichée sans warning

--- app/src/main/java/fr/uge/pokedex/components/Pokedex.kt
+++ app/src/main/java/fr/uge/pokedex/components/Pokedex.kt
@@ -2,28 +2,15 @@ package fr.uge.pokedex.components
 
 
 
-import android.content.ContentValues.TAG
+
 import android.content.Context
-import android.content.res.Resources
-import android.util.DisplayMetrics
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import fr.uge.pokedex.data.Pokemon
