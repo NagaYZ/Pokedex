0fbbaed13acc6f3bfc3dd8a87c96e4c99b187ec6
Zineb SDAF <sdafzineb1s2@gmail.com>
2023-02-16 16:34:03 +0100

    Grille Pokedex affichée

--- app/src/main/java/fr/uge/pokedex/components/Pokedex.kt
+++ app/src/main/java/fr/uge/pokedex/components/Pokedex.kt
@@ -33,58 +33,15 @@ import fr.uge.pokedex.ui.theme.PokedexTheme
 
 @Composable
 fun DisplayPokedex(context: Context, pokemons: List<Pokemon>) {
-    var clicked by rememberSaveable { mutableStateOf(false) }
 
 
-//(Resources.getSystem().displayMetrics.widthPixels/8).dp
+
+
     LazyVerticalGrid(columns = GridCells.Fixed(2), horizontalArrangement = Arrangement.spacedBy(40.dp), verticalArrangement = Arrangement.spacedBy(50.dp), contentPadding = PaddingValues(30.dp, 30.dp)) {
         items(pokemons) {
-            Column(
-                modifier = Modifier
+            PokemonBoxDisplay(pokemon = it, context = context)
 
-                    .border(2.dp, Color.Black)
-                    .clickable {
-                        if (!clicked) {
-                            clicked = true
-                        }
-                    }, horizontalAlignment = Alignment.CenterHorizontally
-            ) {
 
-                Image(
-                    painter = painterResource(it.getIcon(context)),
-                    alignment = Alignment.Center,
-                    contentDescription = it.name,
-                    modifier = Modifier
-                        .width(100.dp)
-                        .height(100.dp)
-                        .border(2.dp, Color.Black)
-                        .padding(0.dp, 20.dp)
-                )
-
-                Text(text = "N°" + String.format("%03d", it.id) + " " + it.name, modifier = Modifier.padding(0.dp,20.dp))
-                Row() {
-                    Image(
-                        painter = painterResource(context.resources.getIdentifier("type_${it.type.first.toString().lowercase()}", "drawable", context.packageName)),
-                        contentDescription = it.type.first.toString(),
-                        modifier = Modifier
-                            .width(60.dp)
-                            .height(60.dp)
-
-                    )
-
-                    if(it.type.second.toString() != "NONE"){
-                        Image(
-                            painter = painterResource(context.resources.getIdentifier("type_${it.type.second.toString().lowercase()}", "drawable", context.packageName)),
-                            contentDescription = it.type.second.toString(),
-                            modifier = Modifier
-                                .width(60.dp)
-                                .height(60.dp)
-
-                        )
-                    }
-                }
-
-            }
         }
     }
 }
