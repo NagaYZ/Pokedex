6be1f370d973b4cfa0fcba52ae3defcb2a942fb5
Zineb SDAF <sdafzineb1s2@gmail.com>
2023-03-02 17:11:14 +0100

    Filtre de type de pokemon corrigé

--- app/src/main/java/fr/uge/pokedex/components/SortByType.kt
+++ app/src/main/java/fr/uge/pokedex/components/SortByType.kt
@@ -5,31 +5,39 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import fr.uge.pokedex.data.Pokemon
 import fr.uge.pokedex.data.PokemonRepository
 import fr.uge.pokedex.data.Type
 
 @Composable
-fun SortByType(aligmt: Alignment, pokemonsByType: (Type) -> List<Pokemon>) {
+fun SortByType(pokemons: List<Pokemon>) : List<Pokemon> {
     val types = Type.values()
-    
+    var resultList by remember {
+        mutableStateOf(pokemons.toMutableList())
+    }
     var state by remember {
         mutableStateOf(false)
     }
-    Box(
+    var currentType by remember {
+        mutableStateOf("")
+    }
+    Row(
         modifier = Modifier.fillMaxWidth(),
-        contentAlignment = aligmt
+
     ) {
         IconButton(onClick = {
             state = true
-        }) {
+        },
+            modifier = Modifier.weight(1/3f)) {
             Row() {
                 Text(text = "Type")
                 Icon(
+
                     imageVector = Icons.Default.ArrowDropDown,
                     contentDescription = "Open Filter"
                 )
@@ -43,34 +51,42 @@ fun SortByType(aligmt: Alignment, pokemonsByType: (Type) -> List<Pokemon>) {
                 types.forEach { nameType ->
                     DropdownMenuItem(
                         onClick = {
-                            pokemonsByType(nameType)
+                            resultList.clear()
+                            currentType = nameType.toString()
                             state = false
                         },
-                        ) {
+                    ) {
                         Text(text = nameType.toString())
                     }
                 }
             }
         }
     }
+    if(currentType != ""){
+        pokemons.forEach { pokemon ->
+            if (pokemon.type.first.toString().equals(currentType) || pokemon.type.second.toString().equals(currentType)) {
+                resultList.add(pokemon)
+            }
+        }
+    }
+    for(i in resultList){
+        println(i.toString())
+    }
+    return resultList
 }
-
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    var list = PokemonRepository(LocalContext.current).getAll().toList()
-    var resultList by remember {
-        mutableStateOf(mutableListOf<Pokemon>())
+    var currentPokemon by remember {
+        mutableStateOf(Pokemon(-1L, "", Pair(Type.NONE, Type.NONE), 0,0,"","", "", true))
     }
-    SortByType(Alignment.TopEnd){
-        println("item $it")
-        list.forEach {  pokemon -> if(pokemon.type.first.equals(it) || pokemon.type.second.equals(it)) resultList.add(pokemon)}
-        for(i in resultList){
-            println("poke $i")
-        }
-        resultList
+    val navController: NavHostController = rememberNavController()
+
+    DisplayPokedex(context = LocalContext.current, pokemons = SortByType(
+
+        pokemons = PokemonRepository(LocalContext.current).getAll().toList())
+        , navController) {
+        currentPokemon = it
     }
-
-
 }
