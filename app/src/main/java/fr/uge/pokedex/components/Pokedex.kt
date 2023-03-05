f3ebde6e6555b521d106ebe4991af1c5f7cb2566
Zineb SDAF <sdafzineb1s2@gmail.com>
2023-03-05 20:53:37 +0100

    Petite modif affichage

--- app/src/main/java/fr/uge/pokedex/components/Pokedex.kt
+++ app/src/main/java/fr/uge/pokedex/components/Pokedex.kt
@@ -20,7 +20,7 @@ import fr.uge.pokedex.database.Profile
 @Composable
 fun DisplayPokedex(sizeGrid: Int = 1, context: Context, pokemons: List<Pokemon>, navController: NavHostController, favorites: List<Favorite>, profile: Profile, getPokemonId: (Long) -> Unit, getPokemonFavoriteId: (Long) -> Unit, clickFavorite : () -> Unit)  {
 
-    LazyVerticalGrid(columns = GridCells.Fixed(sizeGrid), horizontalArrangement = Arrangement.spacedBy(40.dp), verticalArrangement = Arrangement.spacedBy(50.dp), contentPadding = PaddingValues(30.dp, 30.dp)) {
+    LazyVerticalGrid(columns = GridCells.Fixed(sizeGrid), horizontalArrangement = Arrangement.spacedBy(40.dp), verticalArrangement = Arrangement.spacedBy(50.dp), contentPadding = PaddingValues(20.dp)) {
         items(pokemons) {
             favorites.forEach { fav -> if(fav.getPokemonId() == it.id && fav.getProfileId() == profile.getId()) it.isFavorite = true }
             PokemonListDisplay(pokemon = it, context = context, onClick = {
