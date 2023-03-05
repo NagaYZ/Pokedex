929379edbda79d3c3c3a7d8c22876646980d732c
Zineb SDAF <sdafzineb1s2@gmail.com>
2023-03-05 19:47:43 +0100

    Data pokemon chargé dans un launchedEffect

--- app/src/main/java/fr/uge/pokedex/MainActivity.kt
+++ app/src/main/java/fr/uge/pokedex/MainActivity.kt
@@ -1,18 +1,17 @@
 package fr.uge.pokedex
 
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.getValue
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
@@ -30,10 +29,12 @@ import fr.uge.pokedex.database.PokedexAppDatabaseConnection
 import fr.uge.pokedex.database.Profile
 import fr.uge.pokedex.database.ProfileDao
 import fr.uge.pokedex.ui.theme.PokedexTheme
+import fr.uge.pokedex.data.Pokemon
 
 class MainActivity : ComponentActivity() {
     private lateinit var pokemonRepository : PokemonRepository
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -42,10 +43,15 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             PokedexTheme {
-                val profileDao : ProfileDao = PokedexAppDatabaseConnection.connection.profileDao()
-                val favoriteDao : FavoriteDao = PokedexAppDatabaseConnection.connection.favoriteDao()
                 val navController: NavHostController = rememberNavController()
-                val pokemons = PokemonRepository(LocalContext.current).data
+                val pokemons by remember {
+                    mutableStateOf(mutableMapOf<Long, Pokemon>())
+                }
+                val context = LocalContext.current
+                LaunchedEffect(pokemons){
+                    PokemonRepository(context).data.forEach { t, u -> pokemons.put(t, u) }
+                }
+
 
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -60,7 +66,7 @@ class MainActivity : ComponentActivity() {
                     Scaffold(bottomBar = { if(currentRoute != Route.Profiles.path) BottomNavigationMenu(navController)},
                         topBar = { if(currentRoute != Route.Profiles.path) TopBar(navController, currentProfile) }) {
                         Log.d("Padding",it.toString())
-                        NavigationGraph(navController = navController, profileDao, setCurrentProfile = {profile: Profile -> currentProfile = profile }, currentProfile, pokemons = pokemons, favoriteData = favoriteDao)
+                        NavigationGraph(navController = navController, setCurrentProfile = {profile: Profile -> currentProfile = profile }, currentProfile, pokemons = pokemons)
                     }
                 }
 
