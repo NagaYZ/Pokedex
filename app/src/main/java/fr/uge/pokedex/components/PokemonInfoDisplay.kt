0365e167ade0b5be81733b0ad95b589ffda4b993
Gema <theasalexis@gmail.com>
2023-03-10 23:11:02 +0100

    Added display components for pokemon evolutions and pokedex entries

--- app/src/main/java/fr/uge/pokedex/components/PokemonInfoDisplay.kt
+++ app/src/main/java/fr/uge/pokedex/components/PokemonInfoDisplay.kt
@@ -1,12 +1,16 @@
 package fr.uge.pokedex.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -43,14 +47,65 @@ fun PokemonInfoDisplay(
                 flavorText = "Chlorophyll doubles the ability-bearer's Speed during bright sunshine."
             )
         ),
+        evolvesFrom = null,
+        evolvesInto = mutableSetOf(
+            Evolution(
+                species = Pokemon(
+                    1,
+                    "bulbasaur",
+                    icon = fr.uge.pokedex.R.drawable.icon_pkm_1,
+                    sprite = fr.uge.pokedex.R.drawable.pokemon_1
+                ),
+                evolutionTrigger = EvolutionTrigger.LEVEL_UP,
+                minimumLevel = 16,
+                evolvedSpecies = Pokemon(
+                    2,
+                    "ivysaur",
+                    icon = fr.uge.pokedex.R.drawable.icon_pkm_2,
+                    sprite = fr.uge.pokedex.R.drawable.pokemon_2,
+                    evolvesInto = mutableSetOf(
+                        Evolution(
+                            species = Pokemon(
+                                2,
+                                "ivysaur",
+                                icon = fr.uge.pokedex.R.drawable.icon_pkm_2,
+                                sprite = fr.uge.pokedex.R.drawable.pokemon_2
+                            ),
+                            evolvedSpecies = Pokemon(
+                                3,
+                                "venusaur",
+                                icon = fr.uge.pokedex.R.drawable.icon_pkm_3,
+                                sprite = fr.uge.pokedex.R.drawable.pokemon_3
+                            ),
+                            evolutionTrigger = EvolutionTrigger.LEVEL_UP,
+                            minimumLevel = 32
+                        )
+                    )
+                ),
+            )
+        ),
         identifier = "bulbasaur",
         name = "Bulbasaur",
         pokedexEntries = mutableSetOf(
-            FlavorText("A strange seed was planted on its back at birth. The plant sprouts and grows with this POKéMON.", Version.RED),
-            FlavorText("A strange seed was planted on its back at birth. The plant sprouts and grows with this POKéMON.", Version.BLUE),
-            FlavorText("It can go for days without eating a single morsel. In the bulb on its back, it stores energy.", Version.YELLOW),
-            FlavorText("The seed on its back is filled with nutrients. The seed grows steadily larger as its body grows.", Version.GOLD),
-        )
+            FlavorText(
+                "A strange seed was planted on its back at birth. The plant sprouts and grows with this POKéMON.",
+                Version.RED
+            ),
+            FlavorText(
+                "A strange seed was planted on its back at birth. The plant sprouts and grows with this POKéMON.",
+                Version.BLUE
+            ),
+            FlavorText(
+                "It can go for days without eating a single morsel. In the bulb on its back, it stores energy.",
+                Version.YELLOW
+            ),
+            FlavorText(
+                "The seed on its back is filled with nutrients. The seed grows steadily larger as its body grows.",
+                Version.GOLD
+            ),
+        ),
+        icon = fr.uge.pokedex.R.drawable.icon_pkm_1,
+        sprite = fr.uge.pokedex.R.drawable.pokemon_1
     )
 ) {
     LazyColumn(
@@ -66,7 +121,10 @@ fun PokemonInfoDisplay(
             PokemonAbilities(pokemon.abilities)
         }
         item {
-            PokedexEntries(pokedexEntries = pokemon.pokedexEntries)
+            PokemonEvolutions(pokemon.evolvesFrom, pokemon.evolvesInto)
+        }
+        item {
+            PokedexEntries(pokemon.pokedexEntries)
         }
     }
 
@@ -81,11 +139,11 @@ private fun PokemonAbilities(abilities: Abilities) {
         Divider()
         TextDisplay(title = abilities.first!!.name, content = abilities.first!!.flavorText)
         Divider()
-        if(abilities.second != null) {
+        if (abilities.second != null) {
             TextDisplay(title = abilities.second!!.name, content = abilities.second!!.flavorText)
             Divider()
         }
-        if(abilities.hidden != null) {
+        if (abilities.hidden != null) {
             TextDisplay(title = abilities.hidden!!.name, content = abilities.hidden!!.flavorText)
             Divider()
         }
@@ -97,8 +155,10 @@ private fun PokemonCharacteristics(pokemon: Pokemon) {
     Column(
         verticalArrangement = Arrangement.spacedBy(10.dp)
     ) {
-        Text(text = "Characteristics", style = MaterialTheme.typography.h5,
-            fontWeight = FontWeight.Bold)
+        Text(
+            text = "Characteristics", style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.Bold
+        )
         Divider()
         InfoDisplay(
             icon = fr.uge.pokedex.R.drawable.phylogenetic, title = "Species",
@@ -160,7 +220,7 @@ private fun InfoDisplay(icon: Int, title: String, content: String) {
             tint = Color.LightGray
         )
         Spacer(modifier = Modifier.width(17.dp))
-        TextDisplay(title = title , content = content)
+        TextDisplay(title = title, content = content)
     }
 }
 
@@ -170,17 +230,17 @@ private fun PokedexEntries(pokedexEntries: MutableSet<FlavorText>) {
         Modifier.fillMaxWidth(),
         verticalArrangement = Arrangement.spacedBy(10.dp)
     ) {
-        Text(text = "Pokédex Entries", style = MaterialTheme.typography.h5,
-            fontWeight = FontWeight.Bold)
+        Text(
+            text = "Pokédex Entries", style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.Bold
+        )
         Divider()
-        for(pokedexEntry in pokedexEntries) {
+        for (pokedexEntry in pokedexEntries) {
             TextDisplay(title = pokedexEntry.version.toString(), content = pokedexEntry.description)
             Divider()
         }
     }
 }
-
-
 
 @Composable
 private fun TextDisplay(title: String, content: String) {
@@ -192,4 +252,75 @@ private fun TextDisplay(title: String, content: String) {
         Spacer(modifier = Modifier.height(2.dp))
         Text(text = content, style = MaterialTheme.typography.body1)
     }
+}
+
+@Composable
+private fun PokemonEvolutions(evolvesFrom: Evolution?, evolvesInto: Set<Evolution>) {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        Text(
+            text = "Evolutions", style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.Bold
+        )
+        Divider()
+        if (evolvesFrom != null) {
+            EvolutionDisplay(evolvesFrom)
+            Divider()
+        }
+        for (evolution in evolvesInto) {
+            EvolutionDisplay(evolution)
+            Divider()
+            if (evolvesFrom == null) {
+                for (finalEvolution in evolution.evolvedSpecies.evolvesInto) {
+                    EvolutionDisplay(finalEvolution)
+                    Divider()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun EvolutionDisplay(evolution: Evolution) {
+    Column(
+        Modifier
+            .fillMaxWidth()
+    ) {
+        Text(
+            text = if (evolution.evolutionTrigger == EvolutionTrigger.LEVEL_UP) {
+                "Level ${evolution.minimumLevel}"
+            } else {
+                evolution.evolutionTrigger.toString()
+            },
+            style = MaterialTheme.typography.subtitle1,
+            fontWeight = FontWeight.Bold
+        )
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .padding(20.dp)
+                .fillMaxWidth()
+        ) {
+            PokemonIcon(iconResource = evolution.species.icon)
+            Icon(
+                imageVector = Icons.Default.ArrowForward,
+                contentDescription = "Right Icon",
+                tint = Color.LightGray
+            )
+            PokemonIcon(iconResource = evolution.evolvedSpecies.icon)
+        }
+    }
+}
+
+@Composable
+private fun PokemonIcon(iconResource: Int) {
+    Image(
+        painter = painterResource(id = iconResource),
+        contentDescription = "Pokemon icon",
+        modifier = Modifier
+            .size(60.dp)
+    )
 }
