dff1a18f675d56870febb9ac413a82809800de26
Mateusz Szczech <szczenalol@gmail.com>
2023-03-16 15:53:36 +0100

    Profile input regex

--- app/src/main/java/fr/uge/pokedex/components/profile/ProfileDialog.kt
+++ app/src/main/java/fr/uge/pokedex/components/profile/ProfileDialog.kt
@@ -1,8 +1,7 @@
 package fr.uge.pokedex.components.profile
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -10,7 +9,9 @@ import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import fr.uge.pokedex.theme.Shapes
 
 @Composable
 fun ProfileDialog(
@@ -23,16 +24,20 @@ fun ProfileDialog(
     Dialog(onDismissRequest = { close.invoke() }) {
 
         Column(
-            modifier = Modifier.background(MaterialTheme.colors.background),
+            modifier = Modifier
+                .background(MaterialTheme.colors.background, Shapes.medium)
+                .padding(20.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.SpaceBetween
         ) {
             var profileName by remember { mutableStateOf("") }
 
-            Text(text = "Please enter new profile name", style = MaterialTheme.typography.h6)
+            Text(text = "Please enter a new profile name", style = MaterialTheme.typography.h6)
+            Spacer(modifier = Modifier.padding(5.dp))
             TextField(value = profileName, onValueChange = {
-                profileName = it
+                if (!it.contains(Regex("[\\n\\t]"))) profileName = it
             })
+            Spacer(modifier = Modifier.padding(5.dp))
             Button(onClick = {
                 onProfileNameAccept.invoke(profileName)
                 close.invoke()
